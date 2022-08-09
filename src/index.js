2ebe6657b0385a1cfdc8e54194f2b9b18953df0e
Your Name <you@example.com>
2022-08-09 15:52:03 +0300

    feat: add task functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split("");
+  function deleteBrackets(array, config) {
+    for (let i = 0; i < array.length; i++) {
+      for (let subarr = 0; subarr < config.length; subarr++) {
+        for (let j = 0; j < config[subarr].length-1; j++) {
+          if (
+            array[i] == config[subarr][j] &&
+            array[i + 1] == config[subarr][j + 1]
+          ) {
+            console.log('array[i] = ' + config[subarr][j] + ' array[i + 1]= ' + config[subarr][j + 1])
+            array.splice(i, 2);
+            console.log(array)
+            deleteBrackets(array, config);
+            
+          }
+        }
+      }
+    }
+    return array.length == 0;
+  }
+  return deleteBrackets(arr, bracketsConfig);
 }
